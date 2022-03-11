42153940c8993f51863467cf40e25b29421fcbf5
Jason Sturges <jason@jasonsturges.com>
2022-03-11 01:23:59 -0600

    Color utils

---
+++ src/utils/ColorUtils.ts
@@ -0,0 +1,42 @@
+/**
+ * Parse value to find integer color
+ *
+ * Supports:
+ *   - Integer, either radix 10 or 16
+ *   - CSS #abc123 hexadecimal strings
+ *   - CSS rgba() strings
+ */
+export function parseColor(value: number | string | undefined): number {
+  // Undefined
+  if (!value) return 0x0;
+
+  // Integer with radix 10 or 16
+  if (Number.isInteger(value)) return <number>value;
+
+  // CSS Hexadecimal String
+  if (value && (<string>value).charAt(0) === "#") {
+    return parseInt((<string>value).substring(1), 16);
+  }
+
+  // CSS rgba()
+  if ((<string>value).startsWith("rgba")) {
+    let re = /^rgba\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/i;
+
+    if (re.test(<string>value)) {
+      const m = (<string>value).match(re);
+      return (
+        ((parseInt(<string>m?.[1]) & 0xff) << 16) | //
+        ((parseInt(<string>m?.[2]) & 0xff) << 8) | //
+        (parseInt(<string>m?.[3]) & 0xff)
+      );
+    }
+  }
+
+  // TODO: hsla()
+  if ((<string>value).startsWith("hsla")) {
+    console.warn("HSLA color not supported (yet)");
+  }
+
+  // Failover
+  return 0x0;
+}
