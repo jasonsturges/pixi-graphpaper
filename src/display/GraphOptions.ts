2dbf7d928e065f19cb816b93716ee61c12f41e8b
Jason Sturges <jason@jasonsturges.com>
2022-03-11 01:24:14 -0600

    Graph options

---
+++ src/display/GraphOptions.ts
@@ -0,0 +1,25 @@
+/**
+ * Optional parameters
+ */
+export type GraphOptions = {
+  backgroundFillAlpha?: number | undefined;
+  backgroundFillColor?: number | undefined;
+  backgroundVisible?: boolean | undefined;
+  graphHeight?: number | undefined;
+  graphWidth?: number | undefined;
+  intermediateGridSize?: number | undefined;
+  intermediateGridVisible?: boolean | undefined;
+  intermediateStrokeAlpha?: number | undefined;
+  intermediateStrokeColor?: number | undefined;
+  intermediateStrokeWeight?: number | undefined;
+  majorGridSize?: number | undefined;
+  majorGridVisible?: boolean | undefined;
+  majorStrokeAlpha?: number | undefined;
+  majorStrokeColor?: number | undefined;
+  majorStrokeWeight?: number | undefined;
+  minorGridSize?: number | undefined;
+  minorGridVisible?: boolean | undefined;
+  minorStrokeAlpha?: number | undefined;
+  minorStrokeColor?: number | undefined;
+  minorStrokeWeight?: number | undefined;
+};
