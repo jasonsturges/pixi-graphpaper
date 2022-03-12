43a5bc33164eb2271795d2a9b5f2949c60c1dd12
Jason Sturges <jason@jasonsturges.com>
2022-03-11 23:01:03 -0600

    Rule

---
+++ src/model/IRule.ts
@@ -0,0 +1,7 @@
+export interface IRule {
+  alpha?: number | undefined;
+  color?: number | undefined;
+  size?: number | undefined;
+  visible?: boolean | undefined;
+  weight?: number | undefined;
+}
---
+++ src/model/Rule.ts
@@ -0,0 +1,21 @@
+import { IRule } from "./IRule";
+
+/**
+ * Rule presentation model, used for major, intermediate, and minor
+ * graph paper rule lines.
+ */
+export class Rule implements IRule {
+  public alpha?: number | undefined;
+  public color?: number | undefined;
+  public size?: number | undefined;
+  public visible?: boolean | undefined;
+  public weight?: number | undefined;
+
+  constructor(options?: Partial<IRule>) {
+    this.alpha = options?.alpha;
+    this.color = options?.color;
+    this.size = options?.size;
+    this.visible = options?.visible;
+    this.weight = options?.weight;
+  }
+}
