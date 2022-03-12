b64b8efda44eb85fb97a6579947a4fdc6ae113dc
Jason Sturges <jason@jasonsturges.com>
2022-03-11 23:02:34 -0600

    Exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 export { AbstractGraphPaper } from "./display/AbstractGraphPaper";
+export { GraphOptions } from "./display/GraphOptions";
 export { GraphPaper } from "./display/GraphPaper";
 export { GraphStyle } from "./constant/GraphStyle";
 export { GraphStyleFactory } from "./factory/GraphStyleFactory";
 export { GraphStyleType } from "./enum/GraphStyleType";
+export { IRule } from "./model/IRule";
 export { parseColor } from "./utils/ColorUtils";
+export { Rule } from "./model/Rule";
